52d8fb42764a3ae6694894373a94379c350a573d
ohjaesik <96814750+ohjaesik@users.noreply.github.com>
2023-01-02 17:14:17 +0900

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ function tagPrint() {
         p.innerText = data[i].name + "의 생일은 " + data[i].birth.substr(0,2) + "년 " + data[i].birth.substr(2,2)
       + "월 " +data[i].birth.substr(4,2)+ "일 " +"입니다"
       }
+      else if(print > 6){
+        p.innerText = "유효하지 않은 숫자입니다.)
+      }
     }})
   
   }
