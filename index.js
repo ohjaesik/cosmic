74ba5f55f367fc344432d25d544acfb154d38add
ohjaesik <ojaesig86@gmail.com>
2023-01-03 19:30:25 +0900

    d

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ function tagPrint() {
         p.innerText = data[i].name + "의 생일은 " + data[i].birth.substr(0,2) + "년 " + data[i].birth.substr(2,2)
       + "월 " +data[i].birth.substr(4,2)+ "일 " +"입니다"
       }
+      else if(print > 6){
+        p.innerText = "유효하지 않은 숫자입니다"
+      }
+      else if(print == ""){
+        p.innerText = "숫자를 입력해주세요"
+      }
     }})
   
   }
