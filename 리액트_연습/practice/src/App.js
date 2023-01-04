8fc69532d362f5e620bdcbc55889bf68ca9ade42
ohjaesik <ojaesig86@gmail.com>
2023-01-04 12:05:45 +0900

    생활코팅 6 이벤트  실습결과

--- 리액트_연습/practice/src/App.js
+++ 리액트_연습/practice/src/App.js
@@ -2,9 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function Header(props) {
-  console.log('props', props, props.title)
   return <header>
-      <h1><a href="/">{props.title}</a></h1>
+      <h1><a href="/" onClick={(event)=>{
+        event.preventDefault();
+        props.onChangeMode();
+      }}>{props.title}</a></h1>
     </header>
 }
 function Nav(props) {
@@ -12,7 +14,11 @@ function Nav(props) {
   ]
   for(let i=0; i<props.topics.length; i++){
     let t = props.topics[i];
-    lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)
+    lis.push(<li key={t.id}>
+      <a id={t.id} href={'/read/'+t.id} onClick={(event) => {
+        event.preventDefault(); // 이벤트 이전 이벤트 삭제
+        props.onChangeMode(event.target.id); // target=> 이벤트를 일으킨 요소를 가리킴.
+      }}>{t.title}</a></li>)
   }
   return       <nav>
         <ol>
@@ -34,8 +40,12 @@ function App() {
   ]
   return (
     <div>
-      <Header title="WEB"></Header>
-      <Nav topics={topics}></Nav>
+      <Header title="WEB" onChangeMode={() => {
+        alert('Header');
+      }}></Header>
+      <Nav topics={topics} onChangeMode={(id) => {
+        alert(id);
+      }}></Nav>
       <Article title="Welcome" body="Hello, WEB"></Article>
 
 
